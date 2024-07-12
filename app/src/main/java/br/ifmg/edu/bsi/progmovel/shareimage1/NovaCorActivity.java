abc6076304780d12c3732d5d3c2c43a94fd656bb
Ana Luiza <analuiza.ogouvea@gmail.com>
2024-07-12 08:43:25 -0300

    questao 1 mudar cor

--- app/src/main/java/br/ifmg/edu/bsi/progmovel/shareimage1/NovaCorActivity.java
+++ app/src/main/java/br/ifmg/edu/bsi/progmovel/shareimage1/NovaCorActivity.java
@@ -1,24 +1,33 @@
 package br.ifmg.edu.bsi.progmovel.shareimage1;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 public class NovaCorActivity extends AppCompatActivity {
+    public static String EXTRA_COR_ATUAL = "br.ifmg.edu.bsi.progmovel.shareimage1.cor_atual";
+    public static String EXTRA_NOVA_COR = "br.ifmg.edu.bsi.progmovel.shareimage1.nova_cor";
 
+    private EditText etCor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nova_cor);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etCor = findViewById(R.id.etCor);
+
+        Intent intent = getIntent();
+        String corAtual = intent.getStringExtra(EXTRA_COR_ATUAL);
+        etCor.setText(corAtual);
+
+    }
+    public void enviarNovaCor(View v) {
+        String novaCor = etCor.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_NOVA_COR, novaCor);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
